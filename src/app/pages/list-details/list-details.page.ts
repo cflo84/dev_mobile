977fb48ed7ad8cf581ffc45892c0ae3d0b415e70
Laura Saulle <laura.saulle@etu.univ-grenoble-alpes.fr>
2021-03-22 18:18:33 +0100

    KICK d'une liste quand on n'a plus de droit dessus

--- src/app/pages/list-details/list-details.page.ts
+++ src/app/pages/list-details/list-details.page.ts
@@ -1,23 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ListService} from "../../services/list.service";
 import {ActivatedRoute, Router} from '@angular/router';
 import {List} from "../../models/list";
 import {Todo} from "../../models/todo";
 import {ModalController} from "@ionic/angular";
 import {CreateTodoComponent} from "../../modals/create-todo/create-todo.component";
-import { Observable } from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import {ShareListComponent} from '../../modals/share-list/share-list.component';
 import {AuthService} from '../../services/auth.service';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
     selector: 'app-list-details',
     templateUrl: './list-details.page.html',
     styleUrls: ['./list-details.page.scss'],
 })
-export class ListDetailsPage implements OnInit {
+export class ListDetailsPage implements OnInit, OnDestroy {
     listId: string;
     list$: Observable<List>;
+    unsubscribe$: Subject<any>;
     access: string;
+    shareModale: HTMLIonModalElement;
+    todoModale: HTMLIonModalElement;
     modalOpened: boolean; // Disable the possibility to open multiple modals
 
     constructor(private listService: ListService,
@@ -30,7 +34,8 @@ export class ListDetailsPage implements OnInit {
     ngOnInit() {
         this.listId = this.route.snapshot.paramMap.get('id');
         this.list$ = this.listService.getOne(this.listId);
-        this.list$.subscribe(l => {
+        this.unsubscribe$ = new Subject<any>();
+        this.list$.pipe(takeUntil(this.unsubscribe$)).subscribe(l => {
             const email = this.auth.user.email;
             if(l.owner === email){
                 this.access = 'RW';
@@ -44,6 +49,8 @@ export class ListDetailsPage implements OnInit {
                     }
                 });
                 if(!stillSharer) {
+                    if(this.shareModale !== null) this.shareModale.dismiss();
+                    if(this.todoModale != null) this.todoModale.dismiss();
                     this.router.navigate(['/']);
                 }
             }
@@ -51,11 +58,16 @@ export class ListDetailsPage implements OnInit {
         this.modalOpened = false;
     }
 
+    ngOnDestroy() {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
+
     async presentModal(list: List) {
         if (this.modalOpened) return;
         this.modalOpened = true;
 
-        const modal = await this.modalController.create({
+        this.todoModale = await this.modalController.create({
             component: CreateTodoComponent,
             cssClass: 'my-custom-class',
             componentProps: {
@@ -63,29 +75,31 @@ export class ListDetailsPage implements OnInit {
             }
         });
 
-        modal.onDidDismiss().then(() => {
+        this.todoModale.onDidDismiss().then(() => {
             this.modalOpened = false;
+            this.todoModale = null;
         })
 
-        return await modal.present();
+        return await this.todoModale.present();
     }
 
     async presentShareModal(list: List) {
         if (this.modalOpened) return;
         this.modalOpened = true;
 
-        const modal = await this.modalController.create({
+        this.shareModale = await this.modalController.create({
             component: ShareListComponent,
             componentProps: {
                 list: list
             }
         });
 
-        modal.onDidDismiss().then(() => {
+        this.shareModale.onDidDismiss().then(() => {
             this.modalOpened = false;
+            this.shareModale = null;
         })
 
-        return await modal.present();
+        return await this.shareModale.present();
     }
 
     delete(list: List, todo: Todo) {
